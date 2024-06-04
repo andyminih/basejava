2f70950db1b37806ceaaf8ee41d160c0492b0fbf
Andrei Marchenko <marchenko_av@app-sol.ru>
2024-06-04 15:22:40 +0300

    Выполнено ДЗ 9

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -4,21 +4,40 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainFile {
+
+    int depth = 0;
+
     public static void main(String[] args) throws IOException {
         MainFile mainFile = new MainFile();
         mainFile.printDir(new File("."));
     }
 
+
+    void printWithTabs(String string) {
+        System.out.println();
+        for (int i = 0; i < depth; i++) {
+            System.out.print("\t");
+        }
+        System.out.print(string);
+    }
+
     void printDir(File dir) throws IOException {
+
         if (!dir.exists()) {
-            System.out.println("Путь " + dir.getCanonicalPath() + " не найден.");
+            System.out.println("Путь " + dir.getName() + " не найден.");
             return;
         }
         if (dir.isDirectory()) {
-            System.out.println(dir.getCanonicalPath());
+            printWithTabs(dir.getCanonicalPath());
+            depth++;
             for (File file : dir.listFiles()) {
                 printDir((file));
             }
-        } else System.out.println(dir.getCanonicalPath());
+            depth--;
+        } else {
+            printWithTabs(dir.getName());
+        }
     }
+
+
 }
