1626648adea16384aba99af41167e3d395295322
Andrei Marchenko <marchenko_av@app-sol.ru>
2024-06-25 00:21:32 +0300

    Исправлены замечания ДЗ 11 - 1

--- src/com/urise/webapp/util/MainConcurrency.java
+++ src/com/urise/webapp/util/MainConcurrency.java
@@ -1,54 +1,36 @@
 package com.urise.webapp.util;
 
+import static java.lang.Thread.currentThread;
 import static java.lang.Thread.sleep;
 
 public class MainConcurrency {
     private static final Object LOCK_A = new Object();
     private static final Object LOCK_B = new Object();
 
-    public static void main(String[] args) {
+    private static void StartThread(String name, Object lockObject1, Object lockObject2) {
         new Thread(() -> {
-            System.out.println("First thread started");
-            System.out.println("First thread - about to lock LOCK_A");
-            synchronized (LOCK_A) {
-                System.out.println("First thread - LOCK_A locked");
+            System.out.println(currentThread().getName() + " started");
+            System.out.println(currentThread().getName() + " is about to lock " + lockObject1.toString());
+            synchronized (lockObject1) {
+                System.out.println(currentThread().getName() + " has locked " + lockObject1);
                 try {
                     sleep(500);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                System.out.println("First thread - about to lock LOCK_B");
-                synchronized (LOCK_B) {
-                    System.out.println("First thread - LOCK_B locked");
+                System.out.println(currentThread().getName() + " is about to lock " + lockObject2.toString());
+                synchronized (lockObject2) {
+                    System.out.println(currentThread().getName() + " has locked " + lockObject2);
                 }
-                ;
-                System.out.println("First thread - LOCK_B unlocked");
+                System.out.println(currentThread().getName() + " has unlocked " + lockObject2);
             }
-            ;
-            System.out.println("First thread - LOCK_A unlocked");
-            System.out.println("First thread done");
-        }).start();
+            System.out.println(currentThread().getName() + " has unlocked " + lockObject1);
+            System.out.println(currentThread().getName() + " is done");
+        }, name).start();
+    }
 
-        new Thread(() -> {
-            System.out.println("Second thread started");
-            System.out.println("Second thread - about to lock LOCK_B");
-            synchronized (LOCK_B) {
-                System.out.println("Second thread - LOCK_B locked");
-                try {
-                    sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Second thread - about to lock LOCK_A");
-                synchronized (LOCK_A) {
-                    System.out.println("Second thread - LOCK_A locked");
-                }
-                ;
-                System.out.println("Second thread - LOCK_A unlocked");
-            }
-            ;
-            System.out.println("Second thread - LOCK_B unlocked");
-            System.out.println("Second thread done");
-        }).start();
+    public static void main(String[] args) {
+        StartThread("First Thread", LOCK_A, LOCK_B);
+        StartThread("Second Thread", LOCK_B, LOCK_A);
     }
 }
